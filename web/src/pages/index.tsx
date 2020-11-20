95b17dfc5115d004974a7f9fbbe4879bc1d5eab9
oda404 <xdxalexandru404@gmail.com>
2020-11-20 16:23:10 +0200

    Add repos section (non functional)

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,9 @@
-import { Box } from "@chakra-ui/core";
+import { Box, Button, Flex } from "@chakra-ui/core";
 import { FC } from "react";
 import Container from "../components/Container";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { useSelfQuery } from "../generated/graphql";
 
 interface IndexProps
 {
@@ -11,12 +12,62 @@ interface IndexProps
 
 const Index: FC<IndexProps> = () =>
 {
+  const [{data, fetching}] = useSelfQuery();
+
+  let body = null;
+
+  if(!fetching)
+  {
+    if(data?.self)
+    {
+      body = (
+        <>
+          <Flex
+            h="100%" w="300px" bg="#161616" flexDir="column"
+          >
+            <Flex>
+              <Box my="10px" ml="10px" fontSize="19px" color="#e9e9e9">
+                Repos
+              </Box>
+              <Button
+                color="#e9e9e9"
+                my="10px" ml="auto"
+                mr="10px"
+                h="29px"
+                w="70px"
+                display="flex"
+                alignItems="center"
+                padding="10px"
+                fontSize="15px"
+                bg="#212121"
+                border="1px solid #4c4c4c"
+                borderRadius="5px"
+                flexDir="column"
+              >
+                New
+              </Button>
+            </Flex>
+            <Box
+              w="100%"
+              h="1px"
+              bg="#312e2e"
+            />
+          </Flex>
+          <Box
+            h="100%"
+            w="1px"
+            bg="#312e2e"
+          >
+          </Box>
+        </>
+      );
+    }
+  }
+
   return (
     <Container>
       <Header type="full"/>
-      <Box mr="auto" ml="auto" alignSelf="center">
-        Welcome to Gity
-      </Box>
+      {body}
       <Footer/>
     </Container>
   );
