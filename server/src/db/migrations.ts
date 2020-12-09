502c211819a27c1d084db74584915de3e29387fb
oda404 <xdxalexandru404@gmail.com>
2020-12-09 18:03:40 +0200

    Small tweak to repos table creation

--- server/src/db/migrations.ts
+++ server/src/db/migrations.ts
@@ -21,7 +21,7 @@ const CREATE_REPOS_TABLE_QUERY = `\
         "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
         "editedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
         "name" ${REPO_NAME_TYPE} NOT NULL,\
-        "ownerId" BIGINT REFERENCES users(id) ON DELETE CASCADE,\
+        "ownerId" BIGINT REFERENCES "users"("id") ON DELETE CASCADE,\
         "description" ${REPO_DESCRIPTION_TYPE} DEFAULT 'No description provided.',\
         "likes" INT DEFAULT 0,\
         "isPrivate" BOOLEAN DEFAULT FALSE\
