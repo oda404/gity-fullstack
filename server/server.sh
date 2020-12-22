4f061c859e3e43fb74742b9040b7f6af2775c170
oda404 <xdxalexandru404@gmail.com>
2020-12-22 20:18:49 +0200

    Add script for starting the server

---
+++ server/server.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+print_help()
+{
+    echo "Script for starting the server"
+    echo ""
+    echo "By default all services are started in screen sessions"
+    echo "with corresponding names. If either service is manually"
+    echo "specified then only it and any others will be started."
+    echo ""
+    echo "Usage:"
+    echo "  ./server.sh [options]"
+    echo ""
+    echo "Options:"
+    echo "  -d, --deps                 Install deps for all sub projects"
+    echo "  -g, --git-service          Start git-service"
+    echo "  -p, --private-api-service  Start private-api-service"
+    echo "  -P, --public-api-service   Start public-api-service"
+}
+
+if [ $# -eq 0 ]; then
+    pushd git-service/ && tsc --build && popd
+    screen -d -m -S gity-git-service yarn --cwd git-service start
+    pushd private-api-service/ && tsc --build && popd
+    screen -d -m -S gity-private-api-service yarn --cwd private-api-service start
+else
+    for arg in $@
+    do
+        case $arg in
+            "-d" | "--deps")
+                yarn --cwd core/
+                yarn --cwd git-service/
+                yarn --cwd private-api-service/
+                ;;
+            "-h" | "--help")
+                print_help
+                ;;
+            "-g" | "--git-service")
+                pushd git-service/ && tsc --build && popd
+                screen -d -m -S gity-git-service yarn --cwd git-service start
+                ;;
+            "-p" | "--private-api-service")
+                pushd private-api-service/ && tsc --build && popd
+                screen -d -m -S gity-private-api-service yarn --cwd private-api-service start
+                ;;
+            "-P" | "--public-api-service")
+                ;;
+        esac
+    done
+fi
