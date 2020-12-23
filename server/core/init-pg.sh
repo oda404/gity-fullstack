4be4de4308498dd3b4f31ae43f49ce6c97953d12
oda404 <xdxalexandru404@gmail.com>
2020-12-24 00:55:53 +0200

    Better init-pg script

--- server/core/init-pg.sh
+++ server/core/init-pg.sh
@@ -2,12 +2,59 @@
 
 #This script should be run on a fresh db installation
 
-if [ $# -eq 0 ]; then
-    echo "No password for user gity was provided"
-else
-    tsc --build
-    ./migrations.sh
-    ./functions.sh
-    PASS="$1" ./create-gity-user.sh
-    ./constraints.sh
+parsed_args=$(getopt -o d:p:h --long db-pass:,pass:,help -- "$@")
+eval set -- $parsed_args
+
+print_help()
+{
+    echo "Script for setting up the database"
+    echo ""
+    echo "Usage:"
+    echo "  ./init-pg [options]"
+    echo ""
+    echo "Options:"
+    echo "  -h, --help             Prints this message"
+    echo "  -d, --db-pass <pass>   Sets DB_PASS"
+    echo "  -p, --pass    <pass>   Sets PASS"
+}
+
+while :
+do
+    case "$1" in
+        "-h" | "--help")
+            print_help
+            exit
+            ;;
+
+        "-d" | "-db-pass")
+            DB_PASS="$2"
+            shift 2
+            ;;
+
+        "-p" | "-pass")
+            PASS="$2"
+            shift 2
+            ;;
+
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+if [ -z $DB_PASS ]; then
+    DB_PASS="pass"
+    echo "Warning DB_PASS was not set." 
 fi
+
+if [ -z $PASS ]; then
+    PASS="123456789"
+    echo "Warning PASS was not set." 
+fi
+
+tsc --build
+DB_PASS="$DB_PASS" ./migrations.sh
+DB_PASS="$DB_PASS" ./functions.sh
+DB_PASS="$DB_PASS" PASS="$PASS" ./create-gity-user.sh
+DB_PASS="$DB_PASS" ./constraints.sh
