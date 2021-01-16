265037b503aea8b6bcd9aa3569bfacdd4dbdc81d
oda404 <xdxalexandru404@gmail.com>
2021-01-16 22:03:03 +0200

    Better create-default-conf.sh

--- server/core/create-default-conf.sh
+++ server/core/create-default-conf.sh
@@ -1,13 +1,32 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 GITY_CONF_DIR=/etc/gity
+CONF_FILES=(
+    "git" 
+    "pg" 
+    "redis" 
+    "entities/repo" 
+    "entities/user"
+)
 
 echo "Creating default configurations in $GITY_CONF_DIR ..."
-sudo mkdir $GITY_CONF_DIR
-sudo mkdir $GITY_CONF_DIR/entities
-sudo cp $(dirname $BASH_SOURCE)/conf/pg-default.json $GITY_CONF_DIR/pg.json
-sudo cp $(dirname $BASH_SOURCE)/conf/redis-default.json $GITY_CONF_DIR/redis.json
-sudo cp $(dirname $BASH_SOURCE)/conf/git-default.json $GITY_CONF_DIR/git.json
-sudo cp $(dirname $BASH_SOURCE)/conf/enitites/user-default.json $GITY_CONF_DIR/entities/user.json
-sudo cp $(dirname $BASH_SOURCE)/conf/enitites/repo-default.json $GITY_CONF_DIR/entities/repo.json
+sudo mkdir $GITY_CONF_DIR 2> /dev/null
+sudo mkdir $GITY_CONF_DIR/entities 2> /dev/null
+
+for conf_file in "${CONF_FILES[@]}"
+do
+    FILE="$GITY_CONF_DIR/$conf_file"
+    if [ -f "$FILE.json" ]; then
+        printf "File '"$FILE.json"' already exists. Do you want to replace it ? [Y/n] "
+        read response
+        if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
+            sudo cp $(dirname $BASH_SOURCE)/conf/$conf_file-default.json $GITY_CONF_DIR/$conf_file.json
+        else
+            continue
+        fi
+    else
+        sudo cp $(dirname $BASH_SOURCE)/conf/$conf_file-default.json $GITY_CONF_DIR/$conf_file.json
+    fi
+done
+
 echo "Done."
